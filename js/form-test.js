b71c90e2025f0d323225e0f1c0bcdd9bf9059dbb
Boo-urns <coreyaburns@gmail.com>
2013-06-05 14:32:47 -0500

    Finally adding my project to github

---
+++ js/form-test.js
@@ -0,0 +1,105 @@
+'use strict';
+var app = angular.module('myApp', []);
+
+
+app.controller( 'FormCtrl', function FormCtrl($scope, $http, storage) {
+
+	$scope.defaults = {
+											name: 'Test',
+											boilTime: 65,
+											boilSchedule: [],
+											vibrate: 'y',
+											getSchedule: function() { return this.boilSchedule; }
+										};
+
+	$scope.addDefaults = {
+                          unit: true,
+													evTime: $scope.defaults.boilTime
+												};
+
+  $scope.user 		= $scope.defaults;
+  $scope.master   = storage.getItem('master');
+
+
+
+  $scope.save = function(user) {
+    $scope.master= angular.copy(user);
+    storage.setItem('master', $scope.master);
+  };
+
+  $scope.reset = function() {
+    $scope.user   = $scope.defaults;
+    $scope.master = {};
+    storage.clear();
+  };
+
+
+  // BOIL EVENTS
+  $scope.addEvent = function(user) {
+   
+    var unit;
+    var amt  = $scope.amount;
+
+    if(amt != undefined) {
+       unit = $scope.addDefaults.unit === true ? 'oz' : 'g';
+    } else {
+      unit = '';
+    }
+
+    $scope.user.boilSchedule.push({ 
+       amt: amt,
+      unit: unit,
+      name: $scope.searchHops.name, 
+      time: $scope.addDefaults.evTime, 
+      done: false
+    });
+    
+    $scope.evName = '';
+  };
+
+  $scope.deleteEvent = function(user, index){
+    $scope.user.boilSchedule.splice(index, 1);
+  };
+  
+
+  
+  // HOPS
+  $http.get('json/hops.json').success(function(data) {
+    $scope.hops = data;
+  });
+
+  // hopChange() is setup to reshow the ul of the hop matches! 
+  $scope.hopChange = function() {
+    $scope.clicked = false;
+  };
+
+  // hopMatch() a user clicks on the hop they want, hides the list.
+  $scope.hopMatch = function(hop){
+    $scope.searchHops.name = hop;
+    $scope.clicked = true;
+  };
+
+
+});
+
+
+
+
+app.service('storage', function() {
+
+  this.setItem    = function(key, val) {
+    localStorage.setItem(key, JSON.stringify(val));
+  };
+
+  this.getItem    = function(key) {
+    return JSON.parse(localStorage.getItem(key));
+  };
+
+  this.clear      = function() {
+    localStorage.clear();
+  };
+
+  this.removeItem = function(key) {
+    localStorage.removeItem(key);
+  }
+});
